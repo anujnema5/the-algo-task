eb01e2177a2a6365b004d59d0a08fbf6261ec3ff
anujnema5 <anujnema63@gmail.com>
2024-11-14 13:11:03 +0530

    bell-icon-notificaion-alert added

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -18,7 +18,10 @@ const Header = () => (
             </div>
         </div>
         <div className="profile">
-            <FaBell className="notification-icon" size={24} />
+            <div className="" style={{position: "relative"}}>
+                <FaBell className="notification-icon" size={22} />
+                <div className="notification-alert-red"></div>
+            </div>
             <div style={{ height: "30px", width: "2px", backgroundColor: "#E6E6E6" }}></div>
             <img
                 src="https://img.freepik.com/premium-photo/3d-illustration-medical-doctor_864601-353.jpg?w=360"
